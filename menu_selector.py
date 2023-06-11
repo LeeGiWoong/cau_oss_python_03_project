4c4abf9a3e35f3dad7bb97983dff8c69afb5d3c3
LeeGiWoong <fear369@cau.ac.kr>
2023-06-11 18:18:09 +0900

    version#2

--- menu_selector.py
+++ menu_selector.py
@@ -1,4 +1,15 @@
+# 'read_file' 함수를 사용하기 위해 file_manager 모듈을 import합니다.
+# 'str_list_to_class_list'와 'print_spots' 함수를 사용하기 위해
+#  parking_spot_manger 모듈을 import합니다.
+import file_manager
+import parking_spot_manager
+
 def start_process(path):
+    # 주어진 'path' 경로의 파일을 읽고, 그 결과를 'str_list'에 할당합니다.
+    # str_list_to_class_list 함수를 사용하여 'str_list'를 주차 공간 객체의 리스트인 spots로 변환합니다.
+    str_list = file_manager.read_file(path)
+    spots = parking_spot_manager.str_list_to_class_list(str_list)
+    
     while True:
         print("---menu---")
         print("[1] print")
@@ -7,8 +18,8 @@ def start_process(path):
         print("[4] exit")
         select = int(input('type:'))
         if select == 1:
-            print("not implemented yet")
-            # fill this block
+            # 'print_spots' 함수를 호출합니다.
+            parking_spot_manager.print_spots(spots)
         elif select == 2:
             print("---filter by---")
             print("[1] name")
@@ -52,7 +63,8 @@ def start_process(path):
                 # fill this block
             else: print("invalid input")
         elif select == 4:
-            print("not implemented yet")
-            # fill this block
+            # 'Exit'를 출력하고 while문을 빠져나와 프로그램을 종료합니다.
+            print("Exit")
+            break
         else:
             print("invalid input")
--- parking_spot_manager.py
+++ parking_spot_manager.py
@@ -1,12 +1,63 @@
 class parking_spot:
     # you have to implement 'constructor(생성자)' and 'get' method
+    # 생성자로 매개변수를 받아 객체의 속성을 초기화합니다.
+    #  __item 속성은 딕셔너리로 초기화하고, 매개변수로 전달받은 값들을 딕셔너리의 키와 값으로 설정합니다.
+    def __init__(self, name, city, district, ptype, longitude, latitude):
+        self.__item = {
+            'name': name,
+            'city': city,
+            'district': district,
+            'ptype': ptype,
+            'longitude': longitude,
+            'latitude': latitude
+        }
+
+    # 매개변수 'keyword'에 해당하는 값을 반환합니다. 기본인수는 'name'입니다.
+    def get(self, keyword='name'):
+        return self.__item.get(keyword)
+
+    
+    # 객체를 문자열로 표현하는 메서드로, 주차장 정보를 포맷에 맞게 출력합니다.
     def __str__(self):
         item = self.__item
-        s  = f"[{item['name']}({item['ptype']})] "
+        s = f"[{item['name']}({item['ptype']})] "
         s += f"{item['city']} {item['district']}"
         s += f"(lat:{item['latitude']}, long:{item['longitude']})"
         return s
+    
+'''
+주차장 정보를 담고 있는 문자열 리스트 'str_list'를 매개변수로 받아, 
+parking_spot 객체의 리스트로 변환하여 반환합니다.
+각 문자열은 순번, 자원명, 시도, 시군구, 주차장 유형, 경도, 위도 데이터를
+저장하고 있으며, 각 데이터는 쉼표로 구분됩니다.
+주차장 객체(spot)를 생성하고 'class_list'에 추가합니다.
+'''
+def str_list_to_class_list(str_list):
+    class_list = []
+    for string in str_list:
+        data = string.split(',')
+        index = data[0].strip()
+        name = data[1].strip() # 문자열 값
+        city = data[2].strip() # 문자열 값
+        district = data[3].strip() # 문자열 값
+        ptype = data[4].strip() # 문자열 값
+        longitude = float(data[5].strip()) # 실수 값
+        latitude = float(data[6].strip()) # 실수 값
 
+        spot = parking_spot(name, city, district, ptype, longitude, latitude)
+        class_list.append(spot)
+
+    return class_list
+
+'''
+매개변수로 'parking_spot' 클래스 객체의 리스트 spots을 받습니다.
+리스트의 길이(spots 원소의 개수)와 리스트 내용(모든 객체의 값)을 출력합니다.
+'''
+def print_spots(spots):
+    length = len(spots)
+    print(f"---print elements({length})---")
+    for spot in spots:
+        print(spot)
 
 # 각 단계별로 테스트 (테스트할때 주석해제 후 사용)
 if __name__ == '__main__':
